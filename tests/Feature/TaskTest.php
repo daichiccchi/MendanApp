502941ed73a3e6784460c6ddb0e536c765107785
daichiccchi <e5khtamcx@gmail.com>
2022-01-24 08:37:53 +0900

    clud action

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -14,7 +14,7 @@ class TaskTest extends TestCase
     /**
      * @test
      */
-    public function 一覧を取得()
+    public function 一覧を取得できる()
     {
         $tasks = Task::factory()->count(10)->create();
         
@@ -22,5 +22,53 @@ class TaskTest extends TestCase
         $response->assertOk()
                  ->assertJsonCount($tasks->count());
         
+    }
+
+      /**
+     * @test
+     */
+    public function 登録することができる()
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+
+
+        $response = $this->postJson('api/tasks', $data);
+
+        $response
+            ->assertCreated()
+            ->assertJsonFragment($data);
+        
+    }
+
+      /**
+     * @test
+     */
+    public function 更新することができる()
+    {
+        $task = Task::factory()->create();
+        $task->title = '書き換え';
+
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+        
+    }
+
+      /**
+     * @test
+     */
+    public function 削除することができる()
+    {
+        $tasks = Task::factory()->count(10)->create();
+        $task = $tasks->last();
+        $response = $this->deleteJson("api/tasks/{$task->id}");
+        $response->assertOk();
+        
+        $response = $this->getJson("api/tasks");
+        $response->assertJsonCount($tasks->count() -1);
     }
 }
